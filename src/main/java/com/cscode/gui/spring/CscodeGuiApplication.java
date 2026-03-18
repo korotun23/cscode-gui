2df6a8d3f699700f612c8dd28829215eea4ae1b5
Korotun <jo.biliec23@gmail.com>
2026-03-18 22:08:32 +0000

    First commit - spring and javafx setup

---
+++ src/main/java/com/cscode/gui/spring/CscodeGuiApplication.java
@@ -0,0 +1,16 @@
+package com.cscode.gui.spring;
+
+import com.cscode.gui.fx.CscodeGuiFxApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscodeGuiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CscodeGuiFxApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/cscode/gui/spring/CscodeGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cscode.gui.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscodeGuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
